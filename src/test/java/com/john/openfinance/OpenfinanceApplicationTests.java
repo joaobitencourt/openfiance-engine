86b091a3e3de78bcb685a0e8e8415618462240d0
joaobitencourt <jobitencourt924@gmail.com>
2026-03-13 10:32:24 -0300

    fist commit

---
+++ src/test/java/com/john/openfinance/OpenfinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.john.openfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenfinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
