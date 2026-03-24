4032e4c672555f7ed2818410dd17eda6f628a70b
joaobitencourt <jobitencourt924@gmail.com>
2026-03-24 19:25:54 -0300

    add jackson dependency

--- src/main/java/com/john/openfinance/OpenfinanceApplication.java
+++ src/main/java/com/john/openfinance/OpenfinanceApplication.java
@@ -2,8 +2,9 @@ package com.john.openfinance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class OpenfinanceApplication {
 
 	public static void main(String[] args) {
